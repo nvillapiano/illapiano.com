922f3bfa622bdd335b94228cdd9bd556ef478555
Nicholas Villapiano <nicholas.villapiano@ibm.com>
2020-08-11 12:09:09 -0400

    Strings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,34 +4,33 @@
 
 <script>
 // @ is an alias to /src
-import Circles from '@/components/Circles.vue'
+import Circles from "@/components/Circles.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    Circles
+    Circles,
   },
   methods: {
     consoleInfo: function() {
-
-console.log( ___   ___      ___      _______);
-console.log(|   | |   |    |   |    |   _   |);
-console.log(|   | |   |    |   |    |  |_|  |);
-console.log(|   | |   |    |   |    |       |);
-console.log(|   | |   |___ |   |___ |       |);
-console.log(|   | |       ||       ||   _   |);
-console.log(|___| |_______||_______||__| |__|);
-console.log( _______  ___   _______  __    _  _______);
-console.log(|       ||   | |   _   ||  |  | ||       |);
-console.log(|    _  ||   | |  |_|  ||   |_| ||   _   |);
-console.log(|   |_| ||   | |       ||       ||  | |  |);
-console.log(|    ___||   | |       ||  _    ||  |_|  |);
-console.log(|   |    |   | |   _   || | |   ||       |);
-console.log(|___|    |___| |__| |__||_|  |__||_______|);
-    }
+      console.log(" ___   ___      ___      _______");
+      console.log("|   | |   |    |   |    |   _   |");
+      console.log("|   | |   |    |   |    |  |_|  |");
+      console.log("|   | |   |    |   |    |       |");
+      console.log("|   | |   |___ |   |___ |       |");
+      console.log("|   | |       ||       ||   _   |");
+      console.log("|___| |_______||_______||__| |__|");
+      console.log(" _______  ___   _______  __    _  _______");
+      console.log("|       ||   | |   _   ||  |  | ||       |");
+      console.log("|    _  ||   | |  |_|  ||   |_| ||   _   |");
+      console.log("|   |_| ||   | |       ||       ||  | |  |");
+      console.log("|    ___||   | |       ||  _    ||  |_|  |");
+      console.log("|   |    |   | |   _   || | |   ||       |");
+      console.log("|___|    |___| |__| |__||_|  |__||_______|");
+    },
   },
-  mounted () {
+  mounted() {
     this.consoleInfo();
-  }
-}
+  },
+};
 </script>
