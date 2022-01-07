2d8f304b1362c7c098b3a8635bdc934d944fbe65
Nicholas Villapiano <nicholas.villapiano@ibm.com>
2022-01-07 13:29:01 -0500

    Happy with the colorway

--- src/components/Circles.vue
+++ src/components/Circles.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="circles-wrapper">
     <div class="circles">
-      <div v-for="i in 75" :key="i"></div>
+      <div v-for="i in 69" :key="i"></div>
     </div>
   </section>
 </template>
@@ -10,63 +10,13 @@
 export default {
   name: 'Circles',
   methods: {
-    testingCircles: function() {
-      const circlesEl = document.getElementsByClassName('circles-wrapper')[0];
-      const interval = 1000;
-      const initialValue = 500;
-
-      setTimeout( function() {
-        circlesEl.classList.add('grey');
-      }, initialValue + interval * 0);
-
-      setTimeout( function() {
-        circlesEl.classList.add('lavander');
-      }, initialValue + interval * 1);
-
-      setTimeout( function() {
-        circlesEl.classList.add('orange');
-      }, initialValue + interval * 2);
-
-      setTimeout( function() {
-        circlesEl.classList.remove('grey');
-        circlesEl.classList.remove('lavander');
-        circlesEl.classList.remove('orange');
-      }, initialValue + interval * 3);
-    },
     changeBorderColorOnClick: function() {
-      // const circlesEl = document.getElementsByClassName('circles')[0];
       const bodyEl = document.getElementById('app');
-
-      bodyEl.addEventListener("mouseover", (event) => {
-        // const xPosition = event.clientX;
-        // const yPosition = event.clientY;
-        // const r = 255;
-        // const g = 255;
-        // const b = 255;
-        // const rSum = r - xPosition * .1;
-        // const gSum = g - yPosition * .05;
-        // const bSum = b - xPosition * .025 + yPosition * .025;
-        // const max = 255;
-        // const min = 0;
-
-        console.log(event.target.classList)
-
-        // if (rSum <= max && gSum <= max && bSum >= min) {
-        //   event.target.style.borderColor = `rgba(${rSum}, ${gSum}, ${bSum}, 0.5)`;
-        //   console.log(rSum, gSum, bSum);
-        // } else {
-        //   console.log(rSum, gSum, bSum);
-        // }
-
-        // event.target.style.borderColor = `rgba(0, 0, 0, 0.5)`;
+      bodyEl.addEventListener("mouseout", (event) => {
         event.target.classList.add('grey')
-
-        // circlesEl.style.borderColor = `rgba(${r}, ${g}, ${b}, 0.2)`;
-
         setTimeout(() => {
           event.target.classList.remove('grey')
         }, 300);
-
       }, false);
     }
   },
