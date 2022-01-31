baabfadeb5842cbe0b5429581caa22a4cc6d1a60
Nick Villapiano <nvillapiano@gmail.com>
2022-01-31 12:30:51 -0500

    Production release (#3)
    * Updated contact info
    
    * Experimenting with comments
    
    * Attempting to enable comments
    
    * Experimenting in console
    
    * Strings
    
    * More console, removed ESlint rule
    
    * no console fix
    
    * Updated console copy
    
    * Messing around with hover-color gradient
    
    * Color fun
    
    * Happy with the colorway
    
    * Click through variations
    
    Co-authored-by: Nicholas Villapiano <nicholas.villapiano@ibm.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,38 @@
 
 <script>
 // @ is an alias to /src
-import Circles from '@/components/Circles.vue'
+import Circles from "@/components/Circles.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    Circles
-  }
-}
+    Circles,
+  },
+  methods: {
+    consoleInfo: function() {
+      console.log(" ___   ___      ___      _______");
+      console.log("|   | |   |    |   |    |   _   |");
+      console.log("|   | |   |    |   |    |  |_|  |");
+      console.log("|   | |   |    |   |    |       |");
+      console.log("|   | |   |___ |   |___ |       |");
+      console.log("|   | |       ||       ||   _   |");
+      console.log("|___| |_______||_______||__| |__|");
+      console.log(" _______  ___   _______  __    _  _______");
+      console.log("|       ||   | |   _   ||  |  | ||       |");
+      console.log("|    _  ||   | |  |_|  ||   |_| ||   _   |");
+      console.log("|   |_| ||   | |       ||       ||  | |  |");
+      console.log("|    ___||   | |       ||  _    ||  |_|  |");
+      console.log("|   |    |   | |   _   || | |   ||       |");
+      console.log("|___|    |___| |__| |__||_|  |__||_______|");
+      console.log("Let's make something cool together");
+      console.log("nickv@illapiano.com");
+      console.log("--------------------------------");
+      console.log("https://www.doitlewitt.com");
+      console.log("https://citymonsturz.netlify.app/");
+    },
+  },
+  mounted() {
+    this.consoleInfo();
+  },
+};
 </script>
